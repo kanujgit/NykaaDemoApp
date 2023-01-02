cbbdbca5f23da642fb740d2f1d3c45cea51ee688
kanuj30 <anuj.kumar@rakuten.com>
2023-01-02 22:26:12 +0530

    Initial commit

---
+++ app/src/main/java/com/nykaa/nykaademoapp/ProductListActivity.kt
@@ -0,0 +1,15 @@
+package com.nykaa.nykaademoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nykaa.nykaademoapp.databinding.ActivityProductListBinding
+
+class ProductListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductListBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProductListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
